b70347b21de860ea0bb4ca06e6efdbd93bfa151c
oliver <oliverbodden@hotmail.com>
2022-06-27 22:40:36 -0400

    Added default theme logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles';
 
 //pages & components
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import appTheme from './assets/theme';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="pages">
-          <Routes>
-            <Route
-              path="/"
-              element = {<Home />}
-            />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={appTheme}>
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+          <div className="pages">
+            <Routes>
+              <Route
+                path="/"
+                element = {<Home />}
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
